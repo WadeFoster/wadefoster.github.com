fea0cd941bc0081245391b1f0c1fa1c867fd8850
Wade Foster <c.wade.foster@gmail.com>
2012-03-13 18:29:08 -0500

    first content push

---
+++ posts/first.md
@@ -0,0 +1,4 @@
+Date: 2011-3-12
+Title: Welcome to the World
+
+Coming out with a dandy blog for my personal stuff. 
---
+++ settings.py
@@ -0,0 +1,11 @@
+AUTHOR = 'Wade Foster'
+# DISQUS_SITENAME = ''
+GITHUB_URL = 'https://github.com/wadefoster/wadefoster.github.com'
+GOOGLE_ANALYTICS='UA-18066389-2'
+SITEURL = 'http://wadefoster.github.com'
+SITENAME = 'wadefoster'
+SOCIAL = (('twitter', 'http://twitter.com/wadefoster'),
+          ('github', 'https://github.com/wadefoster'),)
+TAG_FEED = 'feeds/%s.atom.xml'
+THEME='notmyidea'
+TWITTER_USERNAME = 'wadefoster'
