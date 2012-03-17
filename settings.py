b084042e0f80f48a7018ebe9f3edbd19e798a97f
Wade Foster <c.wade.foster@gmail.com>
2012-03-17 12:13:49 -0500

    changed settings

--- settings.py
+++ settings.py
@@ -1,11 +1,14 @@
 AUTHOR = 'Wade Foster'
-# DISQUS_SITENAME = ''
+DISQUS_SITENAME = 'wadefoster'
 GITHUB_URL = 'https://github.com/wadefoster/wadefoster.github.com'
-GOOGLE_ANALYTICS='UA-18066389-2'
+GOOGLE_ANALYTICS='UA-30070965-1'
 SITEURL = 'http://wadefoster.github.com'
-SITENAME = 'wadefoster'
+SITENAME = 'Wade Foster'
 SOCIAL = (('twitter', 'http://twitter.com/wadefoster'),
           ('github', 'https://github.com/wadefoster'),)
 TAG_FEED = 'feeds/%s.atom.xml'
 THEME='notmyidea'
 TWITTER_USERNAME = 'wadefoster'
+
+FEED_RSS = 'feeds/all.rss.xml'
+CATEGORY_FEED_RSS = 'feeds/%s.rss.xml'
